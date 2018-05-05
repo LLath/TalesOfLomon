607db21906a4f56a83e7b72471383ba3660818b0
LLath <rothpatrick12@yahoo.de>
2018-05-05 19:18:12 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,8 +1,20 @@
-function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vQgDFPpnHxQzQ44Jo9LtAea4AiACKOixhwg9Zop2auH1TgtEAp5--ugcEllU03XzHPaSiXHwsRA1Vtl/pubhtml',
- callback: function(data, tabletop) { 
- console.log(data)
- },
- simpleSheet: true } )
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
+<script type='text/javascript'> 
+	
+	
+  function init() {
+    Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1kqwdqHWEAbSDSdtm7tDFMSpGqPS5S5IBF_sBjsxOGo4/edit?usp=sharing',
+ 			callback: showInfo,
+			simpleSheet: true } )
+  }
+
+
+function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    console.log(data);
 }
+
 window.addEventListener(‘DOMContentLoaded’, init)
+
+
+</script>
