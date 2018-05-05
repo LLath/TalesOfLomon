229f76e26a0c7650c17ddcd7775520f1f7b890db
LLath <rothpatrick12@yahoo.de>
2018-05-05 22:16:28 +0200

    Create use.js

---
+++ src/use.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+
+<script type="text/javascript">
+
+    //onclick
+    function SpreadsheetOnClick(){
+        document.getElementById("SpreadSheet").createElement("IFRAME");
+    }
+    
+</script>
+</head>
+</html>
