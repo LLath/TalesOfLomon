7ae6408983d7c46de636d122e6cddf094fed5622
LLath <rothpatrick12@yahoo.de>
2018-05-05 22:28:44 +0200

    Update use.js

--- src/use.js
+++ src/use.js
@@ -5,7 +5,11 @@
 
     //onclick
     function SpreadsheetOnClick(){
-        document.getElementById("SpreadSheet").createElement("IFRAME");
+        var x = document.createElement("IFRAME");
+        x.setAttribute("src", 
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vQgDFPpnHxQzQ44Jo9LtAea4AiACKOixhwg9Zop2auH1TgtEAp5--ugcEllU03XzHPaSiXHwsRA1Vtl/pubhtml"
+            );
+        document.body.appendChild(x);
     }
     
 </script>
